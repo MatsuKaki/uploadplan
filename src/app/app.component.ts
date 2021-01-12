bf7c83e6c1af731404f546d6cc41af8cc329911b
MatsuKaki <dang_kim_moana@yahoo.fr>
2021-01-12 12:03:03 +0100

    1 binaries uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,15 @@ import { createWorker } from "tesseract.js";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  img =
+    "https://stackblitz.com/files/uploadplan/github/MatsuKaki/uploadplan/master/src/app/20200623110946_1_PDV-INDICE%20NOTAIRE%201%20-%20CAGES%20A%20ET%20B%20-%20TECHNIQUES%20A001%20(1).png";
+
+  img2 =
+    "https://github.com/MatsuKaki/uploadplan/blob/master/src/app/20200623110946_1_PDV-INDICE%20NOTAIRE%201%20-%20CAGES%20A%20ET%20B%20-%20TECHNIQUES%20A001%20(1).png";
+
+  img3 =
+    "https://github.com/MatsuKaki/ionic-v4-kality/blob/master/src/app/pages/kality/pictures/immeuble1.jpg";
+
   title = "tesseract.js-angular-app";
   ocrResult = "Recognizing...";
   constructor() {
@@ -17,13 +26,12 @@ export class AppComponent {
       logger: m => console.log(m)
     });
     await worker.load();
-    await worker.loadLanguage("fr");
-    await worker.initialize("fr");
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
     const {
       data: { text }
-    } = await worker.recognize(
-      "https://stackblitz.com/files/ionic-v4-kality/github/MatsuKaki/uploadplan/master/src/app/20200623110946_1_PDV-INDICE NOTAIRE 1 - CAGES A ET B - TECHNIQUES A001 (1).JPG"
-    );
+    } = await worker.recognize(this.img3);
+
     this.ocrResult = text;
     console.log(text);
     await worker.terminate();
